afea462f840ff5964e5cbbed6fa11b6bf6e00d2d
Alexander Lopatin <alopatindev@gmail.com>
2016-01-17 17:30:58 +0300

    added more pdf optimizations

--- optpdf.sh
+++ optpdf.sh
@@ -3,15 +3,29 @@
 # optpdf file.pdf
 #   This script will attempt to optimize the given pdf
 #   Looped version of https://tex.stackexchange.com/questions/18987/how-to-make-the-pdfs-produced-by-pdflatex-smaller/199150#199150
-#
+#   and merged with http://www.alfredklomp.com/programming/shrinkpdf/
+
 file=$1
 filebase=$(basename $file .pdf)
 optfile=/tmp/$$-${filebase}_opt.pdf
 
 while :
 do
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH \
-            -sOutputFile=${optfile} ${file}
+    gs \
+      -q -dNOPAUSE -dBATCH -dSAFER         \
+      -sDEVICE=pdfwrite                    \
+      -dCompatibilityLevel=1.3             \
+      -dPDFSETTINGS=/screen                \
+      -dEmbedAllFonts=true                 \
+      -dSubsetFonts=true                   \
+      -dColorImageDownsampleType=/Bicubic  \
+      -dColorImageResolution=72            \
+      -dGrayImageDownsampleType=/Bicubic   \
+      -dGrayImageResolution=72             \
+      -dMonoImageDownsampleType=/Bicubic   \
+      -dMonoImageResolution=72             \
+      -sOutputFile=${optfile} ${file}
+
     if [ $? == '0' ]; then
         optsize=$(stat -c "%s" ${optfile})
         orgsize=$(stat -c "%s" ${file})
